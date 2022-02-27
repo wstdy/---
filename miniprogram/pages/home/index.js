7e9386e2d1407d7b87e6f7f542dcdb2eff3436bf
tianxiang <2546291626@qq.com>
2022-02-27 16:20:50 +0800

    增加音乐

--- miniprogram/pages/home/index.js
+++ miniprogram/pages/home/index.js
@@ -45,9 +45,17 @@ Page({
     }
   },
   onShow() {
-    console.log('this', this)
-    setTimeout(() => {
-      this.setData({ msg: '213' })
-    }, 1000)
-  }
+    this.backmusic()
+  },
+  backmusic() {
+    let back = wx.getBackgroundAudioManager()
+    player()
+    function player() {
+      back.title = "好有意思";   // 必须要有一个title
+      back.src = "http://link.hhtjim.com/163/20953761.mp3";
+      back.onEnded(() => {
+        player()
+      })
+    }
+  },
 })
